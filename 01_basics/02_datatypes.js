56fe891394637f3584a7e748d3b752fe67279ca5
rishav411 <159570012+rishav411@users.noreply.github.com>
2024-02-13 22:20:07 +0545

    datatypes

---
+++ 01_basics/02_datatypes.js
@@ -0,0 +1,21 @@
+"use strict";// traeat all js code as newer version
+//alert(3+3)// we are using node js , not browser
+console.log( 3
+    +3) // code readabilty should be high
+
+console.log("rishav")
+let name = "rishav"
+let age = 18
+let isLoggedIn =false
+let state = null
+// number => 2 to power 53
+//bigint used for big  number
+//string =>""
+//boolean => true/false
+//null => stand alone value (object)
+//undefined =>
+//symbol => unique 
+
+//object 
+console.log(typeof undefined); //undefined
+console.log(typeof null)//object
