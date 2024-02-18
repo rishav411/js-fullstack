81e878bc2f88366fb80a3bb0e8879a9cc4c4986f
rishav411 <159570012+rishav411@users.noreply.github.com>
2024-02-18 22:14:27 +0545

    conersion of data types

---
+++ 01_basics/03_conversionoperation.js
@@ -0,0 +1,17 @@
+let score = 33
+
+
+console.log(typeof score);
+console.log(typeof(score));
+let valueInNumber = Number (score)
+console.log(valueInNumber);
+
+//"33" => 33
+//"33abc"=> NaN
+//true => 1; false=> 0
+let isLoggedIn = "rishav"
+let booleanIsLoggedIn = Boolean (isLoggedIn)
+console.log(booleanIsLoggedIn);
+//1==> true; 0> false
+//""=> false
+//"rishav"=>true
